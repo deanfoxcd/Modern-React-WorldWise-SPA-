3e02fdb23f04d70e4d5a38e3fef7dbcb046ebdfc
Dean Fox <deanfoxcd@gmail.com>
2024-12-06 22:22:50 -0500

    add sidebar and map components

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,14 @@
 import AppNav from '../components/AppNav';
+import Map from '../components/Map';
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css';
 
 export default function AppLayout() {
   return (
-    <div>
+    <div className={styles.app}>
       <AppNav />
+      <Sidebar />
+      <Map />
       <p>App</p>
     </div>
   );
